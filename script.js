8ce0e5f207c8918fb218f68650c2cae93abf7a29
Jad Kreit <>
2021-06-07 15:09:11 +0000

    Added new comments into the JavaScript code

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+/* Sets up variables named 'aInput' 'bInput' 'hInput' and 'area' and assigns the number 0 to each of them */
 let aInput = 0
 let bInput = 0
 let hInput = 0
 let area = 0
-
+/* Sets up a command that listens for a button click on the button with the id 'area-calculate' which then activates the function named 'calculate' */
 document.getElementById('area-calculate').addEventListener('click', calculate)
-
+/* Once the function is activated, it will assign whatever value/number was typed into the input boxes to the variables and turn them into integers to work with. Then, it will take the assigned values of those variables and run through the area of a trapezoid formula and assign the result to the 'area' variable. It'll disable the input boxes so nothing can be changed into them anymore, and it'll display the final result in a new box with the id 'result-section' with the calculation and numbers used. Lastly, the function will change the previous calculate button to a new button with the id 'recalculate' in order to allow the user to make another calculation */
 function calculate () {
   aInput = document.getElementById('a-input').value
   aInput = parseInt(aInput)
@@ -24,9 +25,9 @@ function calculate () {
   document.getElementById('h-result').innerHTML = hInput
   document.getElementById('area-result').innerHTML = area
 }
-
+/* Sets up a command that listens for a button click from the button with the id 'recalculate' and then activated the function called 'newCalculation' */
 document.getElementById('recalculate').addEventListener('click', newCalculation)
-
+/* Once the function is activated, it'll clear whatever numbers were in the input boxes and re-enable them so the user can write new numbers into them. The 'recalculate' button will disappear and the previous button 'calculate' will reappear. Lastly, the box that displays the result will be hidden until the user clicks the 'calculate' button again to display the result */
 function newCalculation () {
   onclick = document.getElementById('a-input').value = ''
   onclick = document.getElementById('b-input').value = ''
